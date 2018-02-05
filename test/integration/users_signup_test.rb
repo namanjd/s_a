11867ccfcfbf8a2e26c5c18c1bab426ae3000096
Naman Doshi <namanatnamostute@gmail.com>
2018-02-05 14:34:12 +0530

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,21 +5,27 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name: "", email: "foo@invalid", password: "foo", password_comfirmation: "bar"} }
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
     end
-    assert_template'users/new'
-    assert_select 'div#<CSS id for error explanation>'
-    assert_select 'div.<CSS class for field with error>'
+    assert_template 'users/new'
+    #assert_select 'div#<CSS id for error explanation>'
+    #assert_select 'div.<CSS class for field with error>'
   end
   
-  test"valid signup information" do
+  test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name: "Example User", email: "foo@valid.com", password: "foobar", password_comfirmation: "foobar"} }
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
     end
-  follow_redirect!
-  assert_template 'users/show'
-  assert_not flash.FILL_IN
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
   end
   
 end
